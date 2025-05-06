e1509ead2f529c8d26391c55dfe849a0e2f88f1e
server194 <server194.newolve@gmail.com>
2025-05-06 12:03:17 +0500

    ajgdajgdl

--- src/pages/CourseDetails.js
+++ src/pages/CourseDetails.js
@@ -1193,6 +1193,7 @@ const ValourAcademy = () => {
                     <source src="https://pub-e58a5f6126d0464c9b810e772987ba18.r2.dev/bannergreen.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
               </video>
+              
               <div className="content-breadcrumb">
                 <span>
                   {selectedLevel.charAt(0).toUpperCase() +
